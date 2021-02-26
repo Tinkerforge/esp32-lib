90d49fdbcbc1a3885a14e06c446645f327280823
Erik Fleckstein <erik@tinkerforge.com>
2021-02-26 14:34:38 +0100

    Add license.

--- src/ringbuffer.h
+++ src/ringbuffer.h
@@ -1,9 +1,20 @@
-/*
- * Copyright (C) 2020 Erik Fleckstein <erik@tinkerforge.com>
+/* esp32-lib
+ * Copyright (C) 2020-2021 Erik Fleckstein <erik@tinkerforge.com>
  *
- * Redistribution and use in source and binary forms of this file,
- * with or without modification, are permitted. See the Creative
- * Commons Zero (CC0 1.0) License for more details.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #pragma once
